d95c84097d8020268f8f69eff9ef6adda002bb7d
EbenezerKobinaAcquah <81479461+EbenezerKobinaAcquah@users.noreply.github.com>
2021-11-29 09:11:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-function clickFunction(){
-    var  eduButton = document.querySelector("#click-me");
-    eduButton.classList.remove("collapsed");
-    eduButton.setAttribute("Aria-expanded", "true");
-    
-    document.querySelector("#question-education").classList.add("show");
-    
-}
 
-document.querySelector("#click-me").addEventListener("CLICK", clickFunction);
