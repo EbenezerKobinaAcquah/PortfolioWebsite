a9c05f11dc7eabccbb47e7b525bd1c93bc5eb1d8
EbenezerKobinaAcquah <81479461+EbenezerKobinaAcquah@users.noreply.github.com>
2021-12-18 10:16:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ function speak () {
 }
 
 document.querySelector("#cv").addEventListener("click", speak);
+
+$(".skills .btn").click(function(){
+    $(".skills .btn").css("z-index", "-1");
+    $(".middle .btn").css("z-index", "-1");
+    $(".skills .close").css("z-index", "1");
+    $(".skills .close").click(function(){
+        $(".skills .btn").css("z-index", "2000");
+        $(".middle .btn").css("z-index", "2000");
+    })
+});
+    
